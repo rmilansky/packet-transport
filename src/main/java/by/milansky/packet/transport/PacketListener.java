b6ab71c8cfaa2e30a4159fd084d8f64487827a60
rmilansky <me@milansky.me>
2023-05-12 20:57:28 +0300

    chore: move duplicate code to utility

--- src/main/java/by/milansky/packet/transport/PacketListener.java
+++ src/main/java/by/milansky/packet/transport/PacketListener.java
@@ -1,6 +1,10 @@
 package by.milansky.packet.transport;
 
-import lombok.*;
+import by.milansky.packet.transport.utility.PacketTransportUtility;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 
 /**
@@ -12,29 +16,17 @@ public abstract class PacketListener<T> {
 
     @SneakyThrows
     public static void setValue(@NonNull Object object, @NonNull String fieldName, @NonNull Object value) {
-        val field = object.getClass().getDeclaredField(fieldName);
-
-        field.setAccessible(true);
-        field.set(object, value);
-        field.setAccessible(false);
+        PacketTransportUtility.setValue(object, fieldName, value);
     }
 
     @SneakyThrows
     public static <A> A getValue(@NonNull Object object, @NonNull String fieldName) {
-        val field = object.getClass().getDeclaredField(fieldName);
-
-        field.setAccessible(true);
-
-        return (A) field.get(object);
+        return PacketTransportUtility.getValue(object, fieldName);
     }
 
     @SneakyThrows
     public static <A> A invoke(@NonNull Object object, @NonNull String methodName, Object... args) {
-        val method = object.getClass().getDeclaredMethod(methodName);
-
-        method.setAccessible(true);
-
-        return (A) method.invoke(object, args);
+        return PacketTransportUtility.invoke(object, methodName, args);
     }
 
     public abstract void accept(@NonNull T t);
